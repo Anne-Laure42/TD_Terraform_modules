94658e3bfe1b47dbe1c4670f3f6558dfd8384f68
Anne-Laure42 <123458466+Anne-Laure42@users.noreply.github.com>
2023-05-23 15:27:57 +0200

    Add files via upload

---
+++ ResourceGroup/variables.tf
@@ -0,0 +1,9 @@
+variable "base_name" {
+    type = string
+    description = "name for the rg and storage account"
+}
+
+variable "location" {
+    type = string
+    description = "location for the deployment"
+}
