ca1394616ddad15b3bae3a5093ca86abc7ddff28
Anne-Laure42 <123458466+Anne-Laure42@users.noreply.github.com>
2023-05-23 15:28:13 +0200

    Add files via upload

---
+++ StorageAccount/outputs.tf
@@ -0,0 +1,3 @@
+output "storage_name_out" {
+    value = resource.azurerm_storage_account.anne_terraform_td_sto.name
+}
---
+++ StorageAccount/variables.tf
@@ -0,0 +1,14 @@
+variable "base_name" {
+    type = string
+    description = "name" 
+}
+
+variable "resource_group_name" {
+    type = string
+    description = "rg name"
+}
+
+variable "location" {
+    type = string
+    description = "location for the deployment"
+}
